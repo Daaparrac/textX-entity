649d202b26e39d7e5401a27183bc053347e8888f
Daniel parra <danielparrac26@gmail.com>
2019-03-05 00:54:22 -0500

    Add files via upload

---
+++ srcgen/Direccion.java
@@ -0,0 +1,34 @@
+public class Direccion {
+
+       private String calle;
+       private String ciudad;
+       private String pais;
+}
+  
+      public Direccion (String calle,  String ciudad,  String pais,  ){
+  }
+
+    public string getCalle(){
+      return this.calle;
+    }
+    public string getCiudad(){
+      return this.ciudad;
+    }
+    public string getPais(){
+      return this.pais;
+    }
+
+      public void setCalle(string value){
+        this.calle = value;
+      }
+
+      public void setCiudad(string value){
+        this.ciudad = value;
+      }
+
+      public void setPais(string value){
+        this.pais = value;
+      }
+
+
+}
